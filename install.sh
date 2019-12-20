b130f15bb9fd0ab93175a0e86af25fea9679d9c8
Cristian E. Nuno <24255819+cenuno@users.noreply.github.com>
2019-12-19 18:16:51 -0800

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-# create pipeline-env from the environment.yml file
-conda env create -f environment.yml
-
-# add pipeline-env as a kernel in jupyter
-python -m ipykernel install --user --name=pipeline-env
